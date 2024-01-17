0e3b553fe522a56afc0a5bc79d16ab8253936644
moisesmedeiros7 <moisesmedeiros@live.com>
2024-01-17 16:10:18 -0300

    Início do módulo 9. Aula 9.3

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	DADOS_INVALIDOS("/dados-invalidos", "Dados Inválidos"),
 	ERRO_DE_SISTEMA ("/erro-de-sistema", "Erro de Sistema"),
 	PARAMETRO_INVALIDO ("/parametro-invalido", "Parâmetro Inválido"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	
 	
 	private String title;
 	private String uri;
