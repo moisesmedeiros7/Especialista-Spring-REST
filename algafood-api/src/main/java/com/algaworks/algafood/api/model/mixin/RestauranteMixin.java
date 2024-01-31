f9d4fb60f323a9f5f15082c2972a260a301956c2
moisesmedeiros7 <moisesmedeiros@live.com>
2024-01-31 13:40:37 -0300

    ...

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -0,0 +1,43 @@
+package com.algaworks.algafood.api.model.mixin;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * {@link RestauranteMixin} é utilizada para setar as configurações
+ * json da classe {@link Restaurante}, deixando o código mais limpo,e
+ * também para setar essas configurações quando não se tem acesso as
+ *  classes do pacote modelo.
+ */
+
+public abstract class RestauranteMixin {
+
+    @JsonIgnoreProperties(value = "nome", allowGetters = true)
+    private Cozinha cozinha;
+    
+    @JsonIgnore
+    private Endereco endereco;
+    
+    @JsonIgnore
+    private LocalDateTime dataCadastro;
+    
+    @JsonIgnore
+    private LocalDateTime dataAtualizacao;
+    
+    @JsonIgnore
+    private List<FormaPagamento> formasPagamento;
+    
+    @JsonIgnore
+    private List<Produto> produtos;
+    
+}        
+
+
--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -27,5 +31,8 @@ public class Cozinha {
 	@NotBlank
 	@Column(nullable = false)
 	private String nome;
+	
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
