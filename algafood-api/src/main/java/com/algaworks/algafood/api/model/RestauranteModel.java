99d58c9ae2934545f3354abd94b35e41ffa7ac2d
moisesmedeiros7 <moisesmedeiros@live.com>
2024-02-01 16:36:45 -0300

    desafio feito

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.api.model;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RestauranteModel {
+
+	private Long id;
+	private String nome;
+	private BigDecimal taxaFrete;
+	private CozinhaModel cozinha;
+
+	/*
+	 * {
+	 * "id": 1,
+	 * "cozinha": {
+	 * 	"id":2,
+	 * 	"nome" : "Japonesa"
+	 * }
+	 */
+
+}
--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -1,6 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +37,7 @@ public class Usuario {
 	 
 	@CreationTimestamp  //cria automaticamente o datetime na CRIAÇÃO da entidade
 	@Column(nullable = false, columnDefinition = "datetime")
-	private LocalDateTime dataCadastro;
+	private OffsetDateTime dataCadastro;
 	
 	@ManyToMany
 	@JoinTable(name = "usuario_grupo",             // <- nome da tabela criada
