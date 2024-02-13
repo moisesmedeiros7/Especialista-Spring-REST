d75bd9caaf914d199c33f4c114a0dd08130a37a9
moisesmedeiros7 <moisesmedeiros@live.com>
2024-02-13 16:33:07 -0300

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -1,10 +1,8 @@
 package com.algaworks.algafood.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.algaworks.algafood.domain.model.FormaPagamento;
 
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+public interface FormaPagamentoRepository extends CustomJpaRepository<FormaPagamento, Long> {
 
 
 
