8083cd0e8c5f9c9a8aa810c5686132b9430a4d77
moisesmedeiros7 <moisesmedeiros@live.com>
2025-03-19 16:07:22 -0300

    Aula 14.06 finalizada

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,14 +27,14 @@ import com.algaworks.algafood.domain.service.CadastroProdutoService;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
-@RequestMapping(value = "/restaurantes/{restauranteId}/produtos")
+@RequestMapping("/restaurantes/{restauranteId}/produtos")
 public class RestauranteProdutoController {
 
 	@Autowired
-	private CadastroProdutoService cadastroProduto;
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
-	private ProdutoRepository produtoRepository;
+	private CadastroProdutoService cadastroProduto;
 	
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
@@ -45,7 +44,6 @@ public class RestauranteProdutoController {
 	
 	@Autowired
 	private ProdutoInputDisassembler produtoInputDisassembler;
-	
 	
 	@GetMapping
 	public List<ProdutoModel> listar(@PathVariable Long restauranteId,
@@ -64,43 +62,36 @@ public class RestauranteProdutoController {
 	}
 	
 	@GetMapping("/{produtoId}")
-    public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
-        Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
-        
-        return produtoModelAssembler.toModel(produto);
-    }
-	 
-	@PostMapping 
-	@ResponseStatus(HttpStatus.CREATED)
-	public ProdutoModel adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
+	public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
 		
+		return produtoModelAssembler.toModel(produto);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ProdutoModel adicionar(@PathVariable Long restauranteId,
+			@RequestBody @Valid ProdutoInput produtoInput) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+		
 		Produto produto = produtoInputDisassembler.toDomainObject(produtoInput);
 		produto.setRestaurante(restaurante);
+		
 		produto = cadastroProduto.salvar(produto);
 		
-		return  produtoModelAssembler.toModel(produto);
+		return produtoModelAssembler.toModel(produto);
 	}
 	
-	@PutMapping("/{produtoId}") 
-	@ResponseStatus(HttpStatus.CREATED)
+	@PutMapping("/{produtoId}")
 	public ProdutoModel atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
 			@RequestBody @Valid ProdutoInput produtoInput) {
-		
 		Produto produtoAtual = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
+		
 		produtoInputDisassembler.copyToDomainObject(produtoInput, produtoAtual);
+		
 		produtoAtual = cadastroProduto.salvar(produtoAtual);
 		
-		return  produtoModelAssembler.toModel(produtoAtual);
+		return produtoModelAssembler.toModel(produtoAtual);
 	}
 	
-	@DeleteMapping("/{produtoId}") 
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
-		
-		cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
-		cadastroProduto.excluir(restauranteId, produtoId);
-	}
-	
-
 }
