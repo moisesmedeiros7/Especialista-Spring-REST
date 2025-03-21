dcb84afdeb4696aee05a24ed36ada0badca3e409
moisesmedeiros7 <moisesmedeiros@live.com>
2025-03-21 13:49:42 -0300

    14.12 desafio

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/FotoProdutoNaoEncontradaException.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.domain.exception;
+
+public class FotoProdutoNaoEncontradaException extends EntidadeNaoEncontradaException {
+
+private static final long serialVersionUID = 1L;
+	
+	public FotoProdutoNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public FotoProdutoNaoEncontradaException(Long restauranteId, Long produtoId) {
+		this(String.format("Não existe um cadastro de foto do Produto ", produtoId, ""
+				+ " do Restaurante ",restauranteId ));
+	}
+
+	
+}
