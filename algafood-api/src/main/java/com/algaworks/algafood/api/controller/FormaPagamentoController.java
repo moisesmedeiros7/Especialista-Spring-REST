bbf164433820d58e046a380a5bdabb2e26b78691
moisesmedeiros7 <moisesmedeiros@live.com>
2025-03-31 22:11:42 -0300

    7.8 - Entendendo e preparando a implementação de Deep ETags

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
@@ -43,6 +46,7 @@ public class FormaPagamentoController {
 	@Autowired
 	private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler; 
 	
+	/*
 	@GetMapping
 	public ResponseEntity<List<FormaPagamentoModel>> listar() {
 		List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
@@ -54,6 +58,36 @@ public class FormaPagamentoController {
 				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
 				.body(formasPagamentosModel);
 	}
+	*/
+	
+	  @GetMapping
+	public ResponseEntity<List<FormaPagamentoModel>> listar(ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+		
+		String eTag = "0";
+		
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+		
+		if (dataUltimaAtualizacao != null) {
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}
+		
+		if (request.checkNotModified(eTag)) {
+			return null;
+		}
+		
+		List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
+		
+		List<FormaPagamentoModel> formasPagamentosModel = formaPagamentoModelAssembler
+				.toCollectionModel(todasFormasPagamentos);
+		
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+				.eTag(eTag)
+				.body(formasPagamentosModel);
+	}
+	  
+	 
 	
 	@GetMapping("/{formaPagamentoId}")
 	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
